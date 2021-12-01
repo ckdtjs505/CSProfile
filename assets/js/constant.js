01854845deae22c68e2b05089a92af20d7202dbc
ckdtjs505 <ckdtjs505@naver.com>
2021-12-02 02:12:03 +0900

    feat: update profile

--- assets/js/constant.js
+++ assets/js/constant.js
@@ -1,32 +1,34 @@
-const fullStackData = 
-    [
-        {
-            title : "📺 afreecaTV",
-            description : "This website is afreecaTV clone",
-            github : "https://github.com/ckdtjs505/csfreecaTV",
-            url : "https://intense-sands-29573.herokuapp.com/"
-        },
-        {
-            title : "✏ guess-mind",
-            description : "This website is chatch mind clone",
-            github : "https://github.com/ckdtjs505/guess-mind",
-            url : "https://intense-ravine-34956.herokuapp.com/"
-        },
-    ]
+const fullStackData = [
+  {
+    title: "📺 afreecaTV",
+    description: "This website is afreecaTV clone",
+    github: "https://github.com/ckdtjs505/csfreecaTV",
+    url: "https://intense-sands-29573.herokuapp.com/"
+  },
+  {
+    title: "✏ guess-mind",
+    description: "This website is chatch mind clone",
+    github: "https://github.com/ckdtjs505/guess-mind",
+    url: "https://intense-ravine-34956.herokuapp.com/"
+  },
+  {
+    title: "🎃 nwitter",
+    description: "This website is twitter clone",
+    github: "https://github.com/ckdtjs505/nwitter",
+    url: "https://ckdtjs505.github.io/nwitter/#/"
+  }
+];
 
+const frontEndData = [
+  {
+    title: "🚗 Shortly",
+    description: "Shortly URL shortening API Challenge",
+    github: "https://github.com/ckdtjs505/frontEnd-practice/tree/master/url-shortening-api-master",
+    url: "https://ckdtjs505.github.io/frontEnd-practice/url-shortening-api-master/index.html"
+  }
+];
 
-const frontEndData = 
-    [
-        {
-            title : "🚗 Shortly",
-            description : "Shortly URL shortening API Challenge",
-            github : "https://github.com/ckdtjs505/frontEnd-practice/tree/master/url-shortening-api-master",
-            url : "https://ckdtjs505.github.io/frontEnd-practice/url-shortening-api-master/index.html"
-        },
-    ]
-
-
-// const frontEndData = 
+// const frontEndData =
 //     [
 //         {
 //             title : "🚗 Shortly",
